a29892620f095f1a3dc3e3657acd756d413eaf06
pechorka <pechor44@gmail.com>
2024-11-10 01:52:14 +0500

    disable quote handling for now. It leads to very big chunks.

--- pkg/textspliter/textspliter.go
+++ pkg/textspliter/textspliter.go
@@ -11,11 +11,10 @@ func SplitText(text string, chunkSize int) []string {
 	buffer.Grow(chunkSize)
 	chunks := make([]string, 0, len(text)/chunkSize)
 
-	inTheQuote := false
 	for i, token := range tokens {
 		buffer.WriteString(token.Value)
 		if buffer.Len() >= chunkSize { // got enough text, search for the nearest end of sentence
-			if token.Type == EndSentence && !inTheQuote {
+			if token.Type == EndSentence {
 				if i+1 < len(tokens) && tokens[i+1].Type == EndSentence { // handle multiple punctuation marks, e.g. "!!!"
 					continue
 				}
@@ -23,12 +22,6 @@ func SplitText(text string, chunkSize int) []string {
 				buffer.Reset()
 				continue
 			}
-		}
-		switch token.Type {
-		case BeginQuote:
-			inTheQuote = true
-		case EndQuote:
-			inTheQuote = false
 		}
 	}
 	if buffer.Len() > 0 {
